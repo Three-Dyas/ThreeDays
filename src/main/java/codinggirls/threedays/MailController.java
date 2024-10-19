b5f4197edcaac4c4752a8b7376bd6dd16d12b590
chxni <jcg3098@naver.com>
2024-10-19 16:37:07 +0900

    [feature] 메일 발송 기능 구현

--- src/main/java/codinggirls/threedays/MailController.java
+++ src/main/java/codinggirls/threedays/MailController.java
@@ -1,11 +1,24 @@
 package codinggirls.threedays;
 
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
 
-    MailService mailService = new MailService();
+    @Autowired
+    MailService mailService;
 
-
+    @PostMapping("/mail/send")
+    public void sendMail(@ModelAttribute MailboxDto mailDto) {
+        try {
+            mailService.sendMail(mailDto);
+            System.out.println("*** 메일 발송 성공 ***");
+        } catch (MessagingException e) {
+            System.out.println("*** 메일 발송 실패 ***");
+        }
+    }
 }
--- src/main/java/codinggirls/threedays/MailService.java
+++ src/main/java/codinggirls/threedays/MailService.java
@@ -1,16 +1,35 @@
 package codinggirls.threedays;
 
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
 public class MailService {
+
+    private final JavaMailSender mailSender;
 
     @Autowired
     MailRepository mailRepository;
 
+    @Async
+    public void sendMail(MailboxDto mailDto) throws MessagingException {
+        System.out.println("Received Email: " + mailDto.getReceivedEmail());
+        System.out.println("Title: " + mailDto.getTitle());
+        System.out.println("Content: " + mailDto.getContent());
 
+        MimeMessage message =mailSender.createMimeMessage();
 
+        message.addRecipients(Message.RecipientType.TO, mailDto.getReceivedEmail());
+        message.setSubject(mailDto.getTitle());
+        message.setText(mailDto.getContent(), "UTF-8");
+        message.setFrom(mailDto.getAuthor() + " <" + mailDto.getAuthorEmail() + ">");
+        mailSender.send(message);
+    }
 }
