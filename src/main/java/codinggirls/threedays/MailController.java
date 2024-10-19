9f1ab8e51d299c5d204e584067ee98dd1961d409
Kim Da Bin <kimbi9912@gmail.com>
2024-10-19 15:01:01 +0900

    structure

---
+++ src/main/java/codinggirls/threedays/MailController.java
@@ -0,0 +1,11 @@
+package codinggirls.threedays;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+
+    MailService mailService = new MailService();
+
+
+}
